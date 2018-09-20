3355cbd13649d34ed0f37b6cd3dd2be30caf7159
Minki Kim <rlaalsrl7777@gmail.com>
2018-09-20 18:53:02 +0900

    [EDIT] delete jade

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var ejs = require('ejs');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -11,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.use(logger('dev'));
 app.use(express.json());
