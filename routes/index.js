7e8f0e84e04e32088c75ec699901255c80cbce1c
Minki Kim <rlaalsrl7777@gmail.com>
2018-11-07 14:41:19 +0900

    [ADD] sequeilize init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index.html', { title: 'Express' });
+});
+
+router.get('/index', function(req, res) {
+  res.redirect('/');
 });
 
 module.exports = router;
