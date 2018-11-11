88cda8bbb8fb1d5fcd5cd69d632f406151142197
Minki-kim95 <rlaalsrl7777@gmail.com>
2018-11-12 01:15:29 +0900

    [ADD] login, logout

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var config = require('../config/config.json')[process.env.NODE_ENV || "development"];
+var models = require('../models');
+var sha256 = require('sha256');
 var router = express.Router();
 var mysql = require('mysql');
 
@@ -10,5 +13,42 @@ router.get('/', function(req, res, next) {
 router.get('/index', function(req, res) {
   res.redirect('/');
 });
-
+//login
+router.post('/login', function(req, res, next) {
+  models.User.findOne({
+    where: {
+      userid: req.body.userid,
+      pw: sha256(req.body.pw)
+    }
+  }).then(function(user){
+    if(user !== null) {
+      req.session.user = user;
+      req.session.user.id = user.id;
+      delete req.body.password;
+      res.send({
+        result: true
+      });
+    } else{
+      res.send({
+        result: false
+      });
+    }
+  });
+});
+router.get('/logout', function(req, res, next) {
+  req.session.user = {};
+  delete req.session.user;
+  if(req.session.user == null){
+    res.send({
+      result: true,
+      msg: 'logout'
+    });
+  } else{
+    res.send({
+      result: false,
+      msg: 'logout fail'
+    });
+  }
+  
+});
 module.exports = router;
