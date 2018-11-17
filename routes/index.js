d9b5a7e035f003c9f94883a98367fceed4e92be9
Minki-kim95 <rlaalsrl7777@gmail.com>
2018-11-18 02:33:31 +0900

    [EDIT] comment of uname in User.js

--- routes/index.js
+++ routes/index.js
@@ -26,11 +26,11 @@ router.post('/login', function(req, res, next) {
       req.session.user.id = user.id;
       delete req.body.password;
       res.send({
-        result: true
+        success: true
       });
     } else{
       res.send({
-        result: false
+        success: false
       });
     }
   });
@@ -38,17 +38,118 @@ router.post('/login', function(req, res, next) {
 router.get('/logout', function(req, res, next) {
   req.session.user = {};
   delete req.session.user;
-  if(req.session.user == null){
-    res.send({
-      result: true,
-      msg: 'logout'
-    });
-  } else{
-    res.send({
-      result: false,
-      msg: 'logout fail'
-    });
-  }
   
 });
+
+router.post('/register', function(req, res, next){
+  console.log(req.body);
+  var result = {};
+  var num_regx = /^[0-9]*$/;
+  var email_regx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // var image_extention_regx = /\.(gif|jpg|jpeg|tiff|png)$/i;
+
+  function getObjectSize(obj) {
+      var size = 0;
+      for (var key in obj)
+          if (obj.hasOwnProperty(key)) size++;
+      return size;
+  }
+  if (req.body.name.length < 1 || req.body.name.length > 10) result = {
+      success: false,
+      text: '최대 10자리까지 가능합니다.'
+  };
+  if (req.body.userid.length < 1 || req.body.userid.length > 10) result = {
+      success: false,
+      text: '학번의 형식이 아닙니다.'
+  };
+  if (req.body.pw.length < 4 || req.body.pw.length > 10) result = {
+      success: false,
+      text: '4~10자리 문자열로 입력바랍니다.'
+  };
+  if (!email_regx.test(req.body.email)) result = {
+      success: false,
+      text: '이메일 형식이 아닙니다.'
+  };
+  if (getObjectSize(result) !== 0) res.send(result);
+  else{
+      models.User.findOne({
+          where: {
+            userid: req.body.userid
+          }
+      }).then(function(user){
+          if (user === null) {
+              req.body.pw = sha256(req.body.pw);
+              models.User.create(req.body).then(function(){
+                result = {
+                    success: true
+                };
+                res.send(result);
+              });
+          } else {
+              result = {
+                  success: false,
+                  text: '이미 존재하는 아이디 입니다.'
+              };
+              res.send(result);
+          }
+      }); 
+  }
+});
+
+router.post('/register/modify', function(req, res, next){
+  console.log(req.body);
+  var result = {};
+  var num_regx = /^[0-9]*$/;
+  var email_regx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // var image_extention_regx = /\.(gif|jpg|jpeg|tiff|png)$/i;
+
+  function getObjectSize(obj) {
+      var size = 0;
+      for (var key in obj)
+          if (obj.hasOwnProperty(key)) size++;
+      return size;
+  }
+  if (req.body.name.length < 1 || req.body.name.length > 10) result = {
+      success: false,
+      text: '최대 10자리까지 가능합니다.'
+  };
+  if (req.body.userid.length < 1 || req.body.userid.length > 10) result = {
+      success: false,
+      text: '학번의 형식이 아닙니다.'
+  };
+  if (req.body.pw.length < 4 || req.body.pw.length > 10) result = {
+      success: false,
+      text: '4~10자리 문자열로 입력바랍니다.'
+  };
+  if (!email_regx.test(req.body.email)) result = {
+      success: false,
+      text: '이메일 형식이 아닙니다.'
+  };
+  if (getObjectSize(result) !== 0) res.send(result);
+  else{
+      models.User.findOne({
+          where: {
+            userid: req.body.userid
+          }
+      }).then(function(user){
+          if (user === null) {
+              req.body.pw = sha256(req.body.pw);
+              user.updateAttributes(req.body).then(function(){
+                  result = {
+                    success: true
+                  };
+                  res.send(result);
+
+              });
+          } else {
+              result = {
+                  success: false,
+                  text: '이미 존재하는 아이디 입니다.'
+              };
+              res.send(result);
+          }
+      }); 
+  }
+});
+
 module.exports = router;
