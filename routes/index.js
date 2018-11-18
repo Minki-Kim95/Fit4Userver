7208d545abd8dbbdcb3bf3de5aacc0dc7590a647
Minki Kim <rlaalsrl7777@gmail.com>
2018-11-18 23:13:48 +0900

    [ADD] upload image (not yet)

--- routes/index.js
+++ routes/index.js
@@ -176,4 +176,38 @@ router.post('/register/modify', function(req, res, next){
   }
 });
 
+//upload fuction
+var upload = function (req, res) {
+  var deferred = Q.defer();
+  var storage = multer.diskStorage({
+    // 서버에 저장할 폴더
+    destination: function (req, file, cb) {
+      cb(null, imagePath);
+    },
+
+    // 서버에 저장할 파일 명
+    filename: function (req, file, cb) {
+      file.uploadedFile = {
+        name: req.params.filename,
+        ext: file.mimetype.split('/')[1]
+      };
+      cb(null, file.uploadedFile.name + '.' + file.uploadedFile.ext);
+    }
+  });
+
+  var upload = multer({ storage: storage }).single('file');
+  upload(req, res, function (err) {
+    if (err) deferred.reject();
+    else deferred.resolve(req.file.uploadedFile);
+  });
+  return deferred.promise;
+};
+
+router.post('/image/:filename', function(req, res, next){
+  upload(req, res).then(function (file) {
+    res.json(file);
+  }, function (err) {
+    res.send(500, err);
+  });
+});
 module.exports = router;
