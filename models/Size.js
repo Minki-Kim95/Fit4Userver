1c28133c491210c23cdcce3445334dac5f4ebb8a
Minki Kim <rlaalsrl7777@gmail.com>
2018-12-06 15:21:45 +0900

    [ADD] size info

--- models/Size.js
+++ models/Size.js
@@ -1,41 +1,75 @@
 module.exports = (sequelize, DataTypes) => {
-    const Size = sequelize.define('Size', {
-      // id: {
-      //   type: DataTypes.STRING,
-      //   allowNull: false,
-      //   primaryKey: true
-      // },
-    //   cid: {
-    //     type: DataTypes.INTEGER,
-    //     allowNull: false,
-    //     comment: '옷 id'
-    //   },
-      topsize: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        comment: '상의 사이즈'
-      },
-      waist: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        comment: '허리둘레'
-      },
-      length: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-        comment: '길이'
-      }
-    }, {
-      tableName: 'size',
-      comment: '옷 사이즈 정보',
-      classMethods: {
-  
-      }
-    });
-  
-    Size.associate = (models) => {
-        Size.belongsTo(models.Clothing, { foreignKey: 'cid', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+  const Size = sequelize.define('Size', {
+    // id: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    //   primaryKey: true
+    // },
+  //   cid: {
+  //     type: DataTypes.INTEGER,
+  //     allowNull: false,
+  //     comment: '옷 id'
+  //   },
+    topsize: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      comment: '상의 사이즈'
+    },
+    top_length: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      comment: '상의 길이'
+    },
+    shoulder_width: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      comment: '어깨 너비'
+    },
+    bust: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      comment: '가슴 단면'
+    },
+    sleeve: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      comment: '소매 길이'
+    },
+    down_length: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      comment: '상의 사이즈'
+    },
+    thigh: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      comment: '허벅지 단면'
+    },
+    rise: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      comment: '밑위'
+    },
+    hem: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      comment: '밑단단면'
+    },
+    waist: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      comment: '허리둘레'
     }
-    return Size;
-  };
-  
+  }, {
+    tableName: 'size',
+    comment: '옷 사이즈 정보',
+    classMethods: {
+
+    }
+  });
+
+  Size.associate = (models) => {
+      Size.belongsTo(models.Clothing, { foreignKey: 'cid', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+  }
+  return Size;
+};
